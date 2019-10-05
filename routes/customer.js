f9d4fe3b7538006b921892838abf2bc26a502e66
cq360767996 <360767996@qq.com>
2019-10-05 14:07:54 +0800

    revert delete bug

--- routes/customer.js
+++ routes/customer.js
@@ -24,7 +24,7 @@ router.put('/edit', async (ctx, next) => {
 });
 
 // 删除
-router.delete('/delete', async (ctx, next) => {
+router.post('/delete', async (ctx, next) => {
     await custService.delete(ctx, next);
 });
 
