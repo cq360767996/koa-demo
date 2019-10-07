5326a1332ab7e2d1b2c1c6999b1d85a881cc6360
cq360767996 <360767996@qq.com>
2019-10-07 18:17:20 +0800

    revert sum bug

--- dao/shopAccDao.js
+++ dao/shopAccDao.js
@@ -1,40 +1,8 @@
 let mysql = require("../sql/mysql");
 
 const shopAccDao = {
-  // getAll: (data, isTotalPage) => {
-  //     let _sql = 'select ';
-  //     if (isTotalPage) {
-  //         _sql += 'count(*) as amount';
-  //     } else {
-  //         _sql += 'b.name, a.*';
-  //     }
-  //     _sql += ' from shop_account a, customer b where a.customer_id = b.id';
-  //     if (data.custName) {
-  //         _sql += " and b.name like '%" + data.custName + "%'";
-  //     }
-  //     if (data.time) {
-  //         _sql += ` and date_format(a.time, '%Y-%m') >= '${data.time[0]}'`;
-  //         _sql += ` and date_format(a.time, '%Y-%m') <= '${data.time[1]}'`;
-  //     }
-  //     if (data.inOrOut) {
-  //         if (data.inOrOut > 0) {
-  //             _sql += " and a.account >= 0";
-  //         } else {
-  //             _sql += " and a.account < 0";
-  //         }
-  //     }
-  //     if (data.keyword) {
-  //         _sql += " and a.matter like '%" + data.keyword + "%'";
-  //     }
-  //     _sql += ' order by a.time desc';
-  //     if (!isTotalPage && data.pageSize && data.currentPage) {
-  //         _sql += ' limit ' + (data.currentPage - 1) * data.pageSize + ', ' + data.pageSize;
-  //     }
-  //     return mysql.query(_sql);
-  // },
   getAll: data => {
-    let _sql = `select a.*, (@balance := @balance + a.account) as balance, c.name 
-    from shop_account a join (select @balance := 0) b join customer c
+    let _sql = `select a.*, c.name from shop_account a join customer c
     where a.customer_id = c.id`;
     _sql += data.custName ? ` and c.name like '%${data.custName}%'` : "";
     _sql += data.time
@@ -46,19 +14,21 @@ const shopAccDao = {
       _sql += data.inOrOut >= 0 ? " and a.account >= 0" : " and a.account < 0";
     }
     _sql += data.keyword ? ` and a.matter like '%${data.keyword}%'` : "";
-    _sql += ` order by a.time asc;`;
+    _sql += ` order by a.time asc`;
     return mysql.query(_sql);
   },
   getTotalPage: data => {
-    let _sql = "select count(*) as amount from shop_account where 1 = 1";
+    let _sql = `select count(*) as amount from shop_account a, customer b
+    where a.customer_id = b.id`;
+    _sql += data.custName ? ` and b.name like '%${data.custName}%'` : "";
     _sql += data.time
-      ? ` and date_format(time, '%Y-%m') >= '${data.time[0]}'
-     and date_format(time, '%Y-%m') <= '${data.time[1]}'`
+      ? ` and date_format(a.time, '%Y-%m') >= '${data.time[0]}'
+     and date_format(a.time, '%Y-%m') <= '${data.time[1]}'`
       : "";
     if (data.inOrOut) {
-      _sql += data.inOrOut >= 0 ? " and account >= 0" : " and account < 0";
+      _sql += data.inOrOut >= 0 ? " and a.account >= 0" : " and a.account < 0";
     }
-    _sql += data.keyword ? ` and matter like '%${data.keyword}%'` : "";
+    _sql += data.keyword ? ` and a.matter like '%${data.keyword}%'` : "";
     return mysql.query(_sql);
   },
   insert: data => {
--- service/shopAccService.js
+++ service/shopAccService.js
@@ -4,32 +4,14 @@ const uuid = require("node-uuid");
 const tips = require("../config/tips");
 
 const shopAccService = {
-  //   getAll: async (ctx, next) => {
-  //     // 查询所有
-  //     let data = ctx.request.body;
-  //     await shopAccDao
-  //       .getAll(data)
-  //       .then(result => {
-  //         let balance = 0;
-  //         let len = result.length;
-  //         for (let i = len - 1; i >= 0; i--) {
-  //           balance += result[i].account;
-  //           result[i].balance = balance;
-  //           // 转换时间格式
-  //           result[i].time = commonService.convertUTCTimeToLocalTime(
-  //             result[i].time
-  //           );
-  //           result[i].time = result[i].time.substring(0, 10);
-  //         }
-  //         commonService.writeData2Ctx(ctx, result);
-  //       })
-  //       .catch(err => {
-  //         console.log(err);
-  //       });
-  //   },
   getAll: async (ctx, next) => {
     let data = ctx.request.body;
     await shopAccDao.getAll(data).then(result => {
+      let balance = 0;
+      result.map(val => {
+        balance += val.account;
+        val.balance = balance;
+      });
       let res = result.reverse();
       res.map(val => {
         val.time = commonService
